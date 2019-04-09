0bff4dce8eac826680c9c07667e7bb7abd641ebc
Leung Ting Pak <aeetpleung@ahkex.asmpt.com>
2019-04-09 15:28:07 +0800

    Updates

---
+++ precache-manifest.9aa1d849816062188d57ca43545b2bf8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b325911a74c4d8228c0a",
+    "url": "/GovVacanciesQuery/static/js/runtime~main.2db170dd.js"
+  },
+  {
+    "revision": "a271638b64b79aa3303a",
+    "url": "/GovVacanciesQuery/static/js/main.074f42ba.chunk.js"
+  },
+  {
+    "revision": "8fd220357b63fe88e6b9",
+    "url": "/GovVacanciesQuery/static/js/2.f81cbded.chunk.js"
+  },
+  {
+    "revision": "a271638b64b79aa3303a",
+    "url": "/GovVacanciesQuery/static/css/main.3bd9272d.chunk.css"
+  },
+  {
+    "revision": "4ea3af8a4444f62dd70733822381a19d",
+    "url": "/GovVacanciesQuery/index.html"
+  }
+];
